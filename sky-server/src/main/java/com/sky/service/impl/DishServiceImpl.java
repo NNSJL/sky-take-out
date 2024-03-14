bcb37fe071c57f6771dd91e926d41b9555c47324
guoruini <1060859719@qq.com>
2024-03-14 23:23:36 +0800

    Add code of adding a new dish;

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,54 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @program: sky-take-out
+ * @description: DishService接口的实现类
+ * @author: grn
+ * @createTime: 2024-03-14 22:19
+ **/
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        //因为Dish表里不需要插入口味数据，所以直接传入实体对象Dish即可
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //向菜品表插入一条数据
+        dishMapper.insert(dish);
+        //获取insert语句生成的主键值
+        Long dishId = dish.getId();
+        //向口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && flavors.size() > 0){
+            //插入之前需要先把dishId放入flavor对象中
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            //批量插入，直接传入list对象
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
